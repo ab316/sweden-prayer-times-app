1a6eca91f94d25ae07ed826fd608b06e5f1c00e6
Abdullah Baig <abdullahbaig456@gmail.com>
2024-12-26 01:03:58 +0100

    Tabs theming

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,24 @@
+import { useTheme } from "@/hooks/ui";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { Tabs } from "expo-router";
 
 export default function TabLayout() {
+  const theme = useTheme();
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: "blue" }}>
+    <Tabs
+      screenOptions={{
+        tabBarActiveBackgroundColor: theme.background,
+        tabBarInactiveBackgroundColor: theme.accent,
+        tabBarActiveTintColor: theme.primaryText,
+        tabBarInactiveTintColor: theme.primaryText,
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: "Home",
           tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name="home" color={color} />
+            <FontAwesome size={28} name="home" color={theme.primaryText} />
           ),
         }}
       />
@@ -18,7 +27,7 @@ export default function TabLayout() {
         options={{
           title: "Qibla",
           tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name="compass" color={color} />
+            <FontAwesome size={28} name="compass" color={theme.primaryText} />
           ),
         }}
       />
@@ -27,7 +36,7 @@ export default function TabLayout() {
         options={{
           title: "About",
           tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name="question" color={color} />
+            <FontAwesome size={28} name="question" color={theme.primaryText} />
           ),
         }}
       />
